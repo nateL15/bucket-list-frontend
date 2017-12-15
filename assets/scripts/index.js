5790dd7d36de524545ac8f7fdb57e81eecd8fcee
Paul Kleinschmidt <32469657+PaulKleinschmidt@users.noreply.github.com>
2017-12-15 17:25:23 -0500

    Revert "Crud"

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,13 +2,9 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-const authEvents = require('./auth/events')
-const listEvents = require('./items/events')
 
 $(() => {
   setAPIOrigin(location, config)
-  authEvents.addHandlers()
-  listEvents.addHandlers()
 })
 
 // use require with a reference to bundle the file and use it in this file
